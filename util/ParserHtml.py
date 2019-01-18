09fc8ae40c7dfce2166f8be541ed138462dd2322
moshiyong <liujing53719>
2019-01-19 02:11:58 +0800

    fix

--- util/ParserHtml.py
+++ util/ParserHtml.py
@@ -27,32 +27,32 @@ def get_page_url():
 def get_image_url():
     for img in get_soup().find_all('img'):
         print(img.get('src'))
-        #if str(img.get('src')).endswith('.jpg'): #and str(img.get('src')).startswith('/'):
+        # if str(img.get('src')).endswith('.jpg'): #and str(img.get('src')).startswith('/'):
         if img.get('src').endswith('.jpg'):
-           # print (img.get('src'))
+            # print (img.get('src'))
             imgg = []
             if str(img.get('src')) not in imgg:
                 imgg.append(str(img.get('src').endswith('.jpg')))
                 for im in imgg:
                     print(im)
-                return imgg
-
-
+               # return imgg
 def create_url():
     pass
+
 
 def save_img():
     i = 1
     for imagepath in get_image_url():
         try:
             f = open('../images/' + str(i) + '.jpg', 'wb')
-            f.write(get_image_url('http://www.jmrenti.org/uploads/allimg/140401/1-140401192442.jpg'))
+            f.write(requests.get('http://www.jmrenti.org/uploads/allimg/140401/1-140401192442.jpg').content)
             f.close()
         except Exception as e:
             print(imagepath + e)
-          #i+1
+        # i+1
 
 
-#for url in get_image_url():
- #   print(url)
-save_img()
+# for url in get_image_url():
+#   print(url)
+#save_img()
+get_image_url()

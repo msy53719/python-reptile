6159ee95edcaf235f69f1a2fd2093e4b6c2fe749
moshiyong <liujing53719>
2019-01-21 19:56:52 +0800

    fix

--- util/ParserHtml.py
+++ util/ParserHtml.py
@@ -5,14 +5,15 @@ import requests
 base_url = 'http://www.jmrenti.org'
 
 
-def get_soup():
-    soup = BeautifulSoup(environment_check.gethtml(base_url), "html.parser")
+def get_soup(url):
+    soup = BeautifulSoup(environment_check.gethtml(url), "html.parser")
     return soup
 
 
 # s=soup.prettify()
-def get_page_url():
-    for link in get_soup().find_all('a'):
+def get_page_all_url(url):
+    page_url=[]
+    for link in get_soup(url).find_all('a'):
         print(link.get('href'))
         if str(link.get('href')).endswith('.html') and str(link.get('href')).startswith('/'):
             # print(link.get('href'))
@@ -24,18 +25,38 @@ def get_page_url():
 
 
 # print(soup.find_all('a'))
-def get_image_url():
-    for img in get_soup().find_all('img'):
-        print(img.get('src'))
-        # if str(img.get('src')).endswith('.jpg'): #and str(img.get('src')).startswith('/'):
-        if img.get('src').endswith('.jpg'):
-            # print (img.get('src'))
-            imgg = []
-            if str(img.get('src')) not in imgg:
-                imgg.append(str(img.get('src').endswith('.jpg')))
-                for im in imgg:
-                    print(im)
-               # return imgg
+def get_image_url(url):
+    imgs = []
+    src_url = []
+    for img in get_soup(url).find_all('img'):
+        # print(img.get('src'))
+        imgs.append(img.get('src'))
+        for src in imgs:
+            if str(src).endswith('.jpg') and str(src).startswith('/'):
+                imgurl = base_url + str(src)
+                if imgurl not in src_url:
+                    src_url.append(imgurl)
+                # print(imgurl)
+    return src_url
+
+
+def get_union_image_url(str):
+    union_url = []
+    for url in str:
+        if url not in get_image_url():
+            union_url.append(url)
+    return union_url
+    # return src_url
+    # if img.get('src').endswith('.jpg'):
+    # print (img.get('src'))
+    #  imgg = []
+    #  if str(img.get('src')) not in imgg:
+    #       imgg.append(str(img.get('src').endswith('.jpg')))
+    #      for im in imgg:
+    #          print(im)
+    # return imgg
+
+
 def create_url():
     pass
 
@@ -45,7 +66,7 @@ def save_img():
     for imagepath in get_image_url():
         try:
             f = open('../images/' + str(i) + '.jpg', 'wb')
-            f.write(requests.get('http://www.jmrenti.org/uploads/allimg/140401/1-140401192442.jpg').content)
+            f.write(requests.get(imagepath).content)
             f.close()
         except Exception as e:
             print(imagepath + e)
@@ -54,5 +75,6 @@ def save_img():
 
 # for url in get_image_url():
 #   print(url)
-#save_img()
-get_image_url()
+# save_img()
+for sre in get_image_url():
+    print(sre)
